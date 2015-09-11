f288ff7d6f34b731dabb2f4ca0c71f2d328fd09d
Kushal Likhi <kushal@innox.io>
2015-09-11 09:11:14 +0530

    added status messages in http server response

--- src/groovy/org/socilab/lgl/server/HTTPServer.groovy
+++ src/groovy/org/socilab/lgl/server/HTTPServer.groovy
@@ -51,19 +51,22 @@ class HTTPServer implements EngineCommandInput {
         handler = new HttpHandler() {
             public void handle(HttpExchange httpExchange) throws IOException {
                 if (httpExchange.getRequestMethod().equalsIgnoreCase("POST")) {
+                    String message
+                    Integer status
                     InputStream is = httpExchange.getRequestBody()
                     String payload = is.getText()
                     Map data = new JsonSlurper().parseText(payload) as Map
-                    String message;
                     if (data.containsKey("source") && data.containsKey("target")) {
                         Boolean resp = inputCommandListener.calculate(data.source as String, data.target as String)
-                        message = resp ? 'OK' : 'ERR'
+                        message = resp ? 'OK' : 'Error in Calculation'
+                        status = resp ? 200 : 203
                     } else {
                         message = 'Invalid JSON body'
+                        status = 204
                     }
                     Headers responseHeaders = httpExchange.getResponseHeaders()
                     responseHeaders.set("Content-Type", "text/plain")
-                    httpExchange.sendResponseHeaders(200, 0)
+                    httpExchange.sendResponseHeaders(status, 0)
                     OutputStream responseBody = httpExchange.getResponseBody()
                     responseBody.write(message.bytes)
                     responseBody.close()
