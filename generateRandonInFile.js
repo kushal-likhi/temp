73abcf3f5ebd2bc5a36f2474696916a0a4c4e556
Kushal Likhi <kushal@innox.io>
2015-09-11 12:56:58 +0530

    updated random test file generator

--- generateRandonInFile.js
+++ generateRandonInFile.js
@@ -1,11 +1,18 @@
-const MAX_NODES = 5000;
-const CONNECTIONS_PER_NODE_MAX = 1000;
-const FILE_NAME = 'generated.txt';
+const MAX_NODES = 500;
+const CONNECTIONS_PER_NODE_MAX = 10;
+const FILE_NAME = 'generated.net';
 var fs = require('fs');
 var fd = fs.openSync(FILE_NAME, 'w');
 
+fs.writeSync(fd, '*Vertices ' + MAX_NODES + '\n');
+fs.writeSync(fd, '*Edges\n');
+
+var dataObj = {nodes: [], links: []};
+
 for (var i = 0; i < MAX_NODES; i++) {
+    dataObj.nodes.push({id: i});
     var numConnections = ~~(Math.random() * CONNECTIONS_PER_NODE_MAX);
+    if(numConnections%2) continue;
     var connected = {};
     console.log(i, numConnections);
     for (var j = 0; j < numConnections; j++) {
@@ -13,8 +20,11 @@ for (var i = 0; i < MAX_NODES; i++) {
         if (!connected[friend]) {
             connected[friend] = true;
             fs.writeSync(fd, "" + i + " " + friend + '\n');
+            dataObj.links.push({source: i, target: friend});
         }
     }
 }
 
 fs.closeSync(fd);
+
+fs.writeFileSync('./generated.json', JSON.stringify(dataObj, null, 4));
